1b759dfc2f020a1c92b6a28fd4f58adfcd0681ce
João Lago <joaolago1996@gmail.com>
2017-04-21 06:03:30 +0100

    adas

---
+++ core/src/com/mygdx/game/controller/gameWorld/WorldStage.java
@@ -0,0 +1,77 @@
+package com.mygdx.game.controller.gameWorld;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.mygdx.game.MyGame;
+import com.mygdx.game.controller.gameWorld.controlEntities.worldEntities.MainCharacterControl;
+import com.mygdx.game.model.modelEntities.worldEntities.MainCharacterModel;
+import com.mygdx.game.screen.GameScreen;
+import com.mygdx.game.screen.visualEntities.worldEntities.MainCharacterView;
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+
+public class WorldStage extends Stage{
+
+    private final World world;
+
+    private MainCharacterControl mainCharacterControl;
+
+    private MyGame game;
+
+    public WorldStage(MyGame mygame){
+        this.game = mygame;
+        this.setViewport(
+                new FitViewport(
+                        (int)(GameScreen.VIEWPORT_WIDTH / GameScreen.PIXEL_TO_METER)
+                        ,(int)(GameScreen.VIEWPORT_WIDTH / GameScreen.PIXEL_TO_METER *  GameScreen.SCREEN_RATIO)
+        ));
+
+        ((OrthographicCamera)this.getCamera()).setToOrtho(false, (int)(GameScreen.VIEWPORT_WIDTH / GameScreen.PIXEL_TO_METER), (int)(GameScreen.VIEWPORT_WIDTH / GameScreen.PIXEL_TO_METER *  GameScreen.SCREEN_RATIO));
+
+        this.world = new World(new Vector2(0,-95f),true);
+        addMainCharacter();
+    }
+
+    public MainCharacterControl getMainCharacter(){
+        return this.mainCharacterControl;
+    }
+
+    private void addMainCharacter(){
+        float posX = (GameScreen.VIEWPORT_WIDTH)/2.0f;
+        float posY = (GameScreen.VIEWPORT_WIDTH*GameScreen.SCREEN_RATIO)/2.0f;
+
+        this.mainCharacterControl = new MainCharacterControl();
+
+        this.mainCharacterControl.createBody(world ,posX ,posY);
+
+        MainCharacterView mainCharacterView = new MainCharacterView(this.game);
+
+        MainCharacterModel mainCharacterModel =
+            new MainCharacterModel(
+                    mainCharacterView
+                    ,this.mainCharacterControl
+            );
+
+        mainCharacterControl.setModel(mainCharacterModel);
+
+        mainCharacterView.setModel(mainCharacterModel);
+
+
+        this.addActor(mainCharacterModel);
+    }
+    @Override
+    public void draw() {
+        super.draw();
+    }
+    @Override
+    public void act(float delta){
+        world.step(delta, 6, 2);
+        super.act(delta);
+    }
+
+
+}
--- core/src/com/mygdx/game/controller/gameWorld/controlEntities/worldEntities/MainCharacterControl.java
+++ core/src/com/mygdx/game/controller/gameWorld/controlEntities/worldEntities/MainCharacterControl.java
@@ -1,16 +1,58 @@
-package com.mygdx.game.controller.World.bodys;
+package com.mygdx.game.controller.gameWorld.controlEntities.worldEntities;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.mygdx.game.controller.EntityControl;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.mygdx.game.controller.gameWorld.controlEntities.EntityControl;
+import com.mygdx.game.model.modelEntities.worldEntities.MainCharacterModel;
 import com.mygdx.game.screen.GameScreen;
 
 
 
-public class MainCharacterControl extends EntityControl{
+public class MainCharacterControl extends EntityControl {
+
+    private InputListener inputListener;
+
+
+    public void calculate(float delta){
+
+        float posX = body.getPosition().x/GameScreen.PIXEL_TO_METER;
+        float posY = body.getPosition().y/GameScreen.PIXEL_TO_METER;
+
+        if(posY<0) {
+            posY = 0;
+
+            body.setTransform(
+                    posX * GameScreen.PIXEL_TO_METER
+                    , 0
+                    , body.getAngle()
+            );
+        }
+        model.setPosition(posX, posY);
+
+    }
+
+    public void jump(){
+        body.setLinearVelocity(body.getLinearVelocity().x,35);
+    }
+
+    public void walkRight(boolean stop){
+        if(!stop)
+            body.setLinearVelocity(20,body.getLinearVelocity().y);
+        else
+            body.setLinearVelocity(0,body.getLinearVelocity().y);
+    }
+
+    public void walkLeft(boolean stop){
+        if(!stop)
+            body.setLinearVelocity(-20,body.getLinearVelocity().y);
+        else
+            body.setLinearVelocity(0,body.getLinearVelocity().y);
+    }
 
     //caracteristicas todas desta classe no mundo
     public Body createBody(World world, float x, float y) {
